4222ca13846bde83361b998c1bd6422a351a9690
Paulo Mouat <paulo.mouat@gmail.com>
2019-08-11 13:10:08 -0400

    Initial commit.

---
+++ MfsGradient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace MfsGradient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MFS Gradient tweaker");
+
+            byte onColor = 65;
+            byte onColorLimit = 126;
+            byte offColor = 1;
+
+            var bytes = File.ReadAllBytes("/Users/paulomouat/Documents/Midi Fighter Twister/cc-toggle-all.mfs");
+
+            var pos = 0;
+            while(pos < bytes.Length)
+            {
+                if (bytes[pos] == 20 && bytes[pos - 2] == 19 && bytes[pos - 4] == 18)
+                {
+                    bytes[pos - 1] = onColor;
+                    bytes[pos + 1] = offColor;
+                    if (onColor < onColorLimit)
+                        onColor++;
+                    offColor++;
+                }
+                Console.WriteLine(pos);
+                pos += 1;
+            }
+
+            File.WriteAllBytes("/Users/paulomouat/Documents/Midi Fighter Twister/gradient.mfs", bytes);
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
